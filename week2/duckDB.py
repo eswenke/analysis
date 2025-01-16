8f6fec8186fa84ad4315d6ea95cd12b4111a5ca8
Ethan Swenke <swenke.ethan.us@gmail.com>
2025-01-15 17:46:42 -0800

    week 2 setup

---
+++ week2/duckDB.py
@@ -0,0 +1,48 @@
+import duckdb as ddb
+import sys
+import gzip
+import csv
+from datetime import datetime
+import time
+
+# check input is valid
+def validate_input(start_date, start_hour, end_date, end_hour):
+    start = datetime.strptime(f"{start_date} {start_hour}", "%Y-%m-%d %H")
+    end = datetime.strptime(f"{end_date} {end_hour}", "%Y-%m-%d %H")
+
+    # check start date is before end date
+
+    if end <= start:
+        raise ValueError("start hour must be before end hour")
+    return start, end
+
+
+def main():
+    # start time
+    start_time = time.perf_counter_ns()
+
+    # validate command line args count
+    if len(sys.argv) != 5:
+        print("Usage: python3 rplace.py <start_date> <start_hour> <end_date> <end_hour>")
+        sys.exit()
+    
+    # get command line args
+    start_date = sys.argv[1]
+    start_hour = sys.argv[2]
+    end_date = sys.argv[3]
+    end_hour = sys.argv[4]
+
+    # validate input and read in data
+    start_hour, end_hour = validate_input(start_date, start_hour, end_date, end_hour)
+    
+    # preprocess data?
+    # 
+    #
+    
+    
+    # get end_time
+    end_time = time.perf_counter_ns()
+
+    
+if __name__ == '__main__':
+    main()
