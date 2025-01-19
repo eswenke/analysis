0d5b4a4b3b8d530c7d23b459e3ecb6a916186849
Ethan Swenke <eswenke@calpoly.edu>
2025-01-18 19:23:58 -0800

    gitingore and preprocess stuff

--- week2/preprocess.py
+++ week2/preprocess.py
@@ -5,29 +5,36 @@ import gzip
 import csv
 from datetime import datetime
 import time
+import shutil
 
 
-def preprocess_data(file_path):
-    # read the CSV file lazily
-    with gzip.open(file_path, mode='rt', encoding='utf-8') as file:
-        df = pl.scan_csv(file.read())
+def unzip(file_path): 
+    with gzip.open(file_path, 'rb') as f_in:
+        with open('2022_place_canvas_history.csv', 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+def preprocess_data(file_path): 
+    with open(file_path, mode='rt', encoding='utf-8') as f:
+        df = pl.scan_csv(f)
         lazy_processed = (
             df
             # parse the datetime column into a proper datetime type
-            .with_columns(pl.col("timestamp").str.strptime(pl.Datetime, format="%Y-%m-%d %H:%M:%S%.f %Z"))
+            .with_columns(pl.col("timestamp").str.strptime(pl.Datetime, format="%Y-%m-%d %H:%M:%S"))
         )
-        print(lazy_processed.head(10).collect())    
+        print(lazy_processed.head(10).collect())
 
-    return
+        return
     
-
 def main():
     # get start time
     start_time = time.perf_counter_ns()
 
-    # preprocess data?
-    preprocess_data("../week1/2022_place_canvas_history.csv.gzip")
-    
+    # unzip
+    # unzip("../week1/2022_place_canvas_history.csv.gzip")
+
+    # preprocess data
+    preprocess_data("2022_place_canvas_history.csv")
+
     # get end_time
     end_time = time.perf_counter_ns()
 
